13def88d3b2f0d62ad8f911b437bcc887fef92fd
blubb12blubb3 <larsknittler@gmail.com>
2023-09-22 15:24:23 +0200

    added /steam

--- cogs/steam.py
+++ cogs/steam.py
@@ -6,9 +6,9 @@ class Steam(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="steam", description="Link to the Official Valheim Steam Page")
+    @app_commands.command(name="steam", description="Link to the Official Valheim Steam Store Page")
     async def steam(self, interaction: discord.Interaction):
-        await interaction.response.send_message("This Command is still in development.", ephemeral=True)
+        await interaction.response.send_message("https://store.steampowered.com/app/892970/Valheim/", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Steam(bot))
