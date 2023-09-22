458cd28d6b6a8167e1fedb6500d15859f43ce11c
blubb12blubb3 <larsknittler@gmail.com>
2023-09-22 15:11:26 +0200

    added /discord

--- cogs/discord.py
+++ cogs/discord.py
@@ -8,7 +8,7 @@ class Discord(commands.Cog):
 
     @app_commands.command(name="discord", description="Link to the Official Valheim Discord Server")
     async def discord(self, interaction: discord.Interaction):
-        await interaction.response.send_message("This Command is still in development.", ephemeral=True)
+        await interaction.response.send_message("https://discord.gg/valheim", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Discord(bot))
