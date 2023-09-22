e8df26178c61cf485026eba9ad519cee11cbaaf6
blubb12blubb3 <larsknittler@gmail.com>
2023-09-22 14:21:15 +0200

    added /help

---
+++ cogs/help.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+import discord
+from discord import app_commands
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="help", description="Get a list of Commands you can use")
+    async def help(self, interaction: discord.Interaction):
+        embed=discord.Embed(title="A list of Commands you can use:", color=0x7289da)
+        #Guide
+        embed.add_field(name="Guide", value="""/wiki - Search about anything on the Valheim Wiki 
+                        /help - Well, it does this...""", inline=False)
+        #Entertainment
+        embed.add_field(name="Entertainment", value="""/music - Spotify Links to Valheim related music 
+                        /quest - Random quests if you are bored""", inline=False)
+        #Official
+        embed.add_field(name="Official", value="""/discord - Discord Server 
+                        /game - Website 
+                        /music - Music Album 
+                        /news - Updates from the developers of Valheim 
+                        /steam - Steam Page""", inline=False)
+        
+        embed.add_field(name="", value="[Bot Support Discord Server](https://discord.gg/DF6HRjzzRh)", inline=True)
+        embed.add_field(name="", value="[Official Valheim Discord Server](https://discord.gg/valheim)", inline=True)
+
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
+        embed.set_footer(text=interaction.user,icon_url=interaction.user.avatar.url)
+        await interaction.response.send_message(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Help(bot))
